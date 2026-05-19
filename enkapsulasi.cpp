1f948f72ba176e8f105f5a14745aa3900eb139ce
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-05-19 19:15:53 +0700

    int mai

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-using namespace std;
-
 class remoteLampu {
 private:
     string saklarNo[10];
@@ -13,3 +10,18 @@ public:
     }
 };
 
+int main() {
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
+}
